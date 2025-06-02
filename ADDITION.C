ec193b68fecc6da16f728514c606dcb6bb9f9c3e
hastigohil89 <hastigohil89@gmail.com>
2025-06-02 09:56:19 -0700

    Add files via upload

---
+++ ADDITION.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+  double a,b,c;
+  clrscr();
+  a=10;
+  b=20;
+  c=a+b;
+  printf("addition: %lf",c);
+  getch();
+  return 0;
+  }
---
+++ IF.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int a;
+  clrscr();
+  printf("\nenter A:");
+  scanf("%d",&a);
+  if(a>0)
+  {
+    printf("\n A is positive number");
+  }
+  else
+  {
+    printf("\n B is negative number");
+  }
+  getch();
+}
