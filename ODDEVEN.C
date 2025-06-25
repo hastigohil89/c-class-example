8a41f886f4df9cda54af5a3c628f78235bcdd18a
hastigohil89 <hastigohil89@gmail.com>
2025-06-24 21:22:26 -0700

    Add files via upload

---
+++ ODDEVEN.C
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<stdio.h>
+
+void main()
+{
+  int a;
+  clrscr();
+  printf("\n enter A:");
+  scanf("%d" ,&a);
+  if(a%2==0)
+  {
+     printf("\n A is even number");
+  }
+  else
+  {
+     printf("\n B is odd number");
+  }
+  getch();
+}
