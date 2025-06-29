2041a2837dd0caf90fb7fab262683b5f576baed1
hastigohil89 <hastigohil89@gmail.com>
2025-06-28 21:51:22 -0700

    Add files via upload

---
+++ PYRAMID.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int i,j,k;
+  clrscr();
+  for(i=9;i>=0;i--)
+ {
+  for(k=0;k<9-i;k++)
+  {
+    printf(" ");
+  }
+  for(j=1;j<=i;j++)
+  {
+    printf("%d",j);
+  }
+   printf("\n");
+ }
+getch();
+}
